ee814a10319ce70f4f5fda54328bb3651461e988
Kirill Tankov <nolixqwerty@mail.ru>
2024-03-04 22:04:37 +0300

    Добавил оставшиеся тесты

--- test_api/test_api.py
+++ test_api/test_api.py
@@ -1,7 +1,7 @@
 import unittest
 import requests
 
-class Test_users(unittest.TestCase):
+class TestUsers(unittest.TestCase):
     def setUp(self):
         self.url = 'https://reqres.in/'
 
@@ -26,3 +26,140 @@ class Test_users(unittest.TestCase):
 
         expected_response_body = {}
         self.assertDictEqual(self.response.json(), expected_response_body)
+
+
+class TestPages(unittest.TestCase):
+    def setUp(self):
+        self.url = 'https://reqres.in/'
+
+    def test_list_resource(self):
+        self.response = requests.get(self.url + 'api/unknown')
+        self.assertEqual(self.response.status_code, 200)
+
+        expected_response_body = {"page":1,"per_page":6,"total":12,"total_pages":2,"data":[{"id":1,"name":"cerulean","year":2000,"color":"#98B2D1","pantone_value":"15-4020"},{"id":2,"name":"fuchsia rose","year":2001,"color":"#C74375","pantone_value":"17-2031"},{"id":3,"name":"true red","year":2002,"color":"#BF1932","pantone_value":"19-1664"},{"id":4,"name":"aqua sky","year":2003,"color":"#7BC4C4","pantone_value":"14-4811"},{"id":5,"name":"tigerlily","year":2004,"color":"#E2583E","pantone_value":"17-1456"},{"id":6,"name":"blue turquoise","year":2005,"color":"#53B0AE","pantone_value":"15-5217"}],"support":{"url":"https://reqres.in/#support-heading","text":"To keep ReqRes free, contributions towards server costs are appreciated!"}}
+        self.assertDictEqual(self.response.json(), expected_response_body)
+
+    def test_single_resource(self):
+        self.response = requests.get(self.url + 'api/unknown/2')
+        self.assertEqual(self.response.status_code, 200)
+
+        expected_response_body = {"data":{"id":2,"name":"fuchsia rose","year":2001,"color":"#C74375","pantone_value":"17-2031"},"support":{"url":"https://reqres.in/#support-heading","text":"To keep ReqRes free, contributions towards server costs are appreciated!"}}
+        self.assertDictEqual(self.response.json(), expected_response_body)
+
+    def test_single_resource_not_found(self):
+        self.response = requests.get(self.url + 'api/unknown/23')
+        self.assertEqual(self.response.status_code, 404)
+
+        expected_response_body = {}
+        self.assertDictEqual(self.response.json(), expected_response_body)
+
+
+class TestUserCRUD(unittest.TestCase):
+    def setUp(self):
+        self.url = 'https://reqres.in/'
+
+    def test_create(self):
+        user_data = {
+            "name": "testir",
+            "job": "tester"
+        }
+
+        self.response = requests.post(self.url + 'api/users', json=user_data)
+        self.assertEqual(self.response.status_code, 201)
+
+        expected_user_data = self.response.json()
+        self.assertEqual(expected_user_data["name"], user_data["name"])
+        self.assertEqual(expected_user_data["job"], user_data["job"])
+
+    def test_update_put(self):
+        user_data = {
+            "name": "test",
+            "job": "tester"
+        }
+
+        self.response = requests.put(self.url + 'api/users/2', json=user_data)
+        self.assertEqual(self.response.status_code, 200)
+
+        expected_user_data = self.response.json()
+        self.assertEqual(expected_user_data["name"], user_data["name"])
+        self.assertEqual(expected_user_data["job"], user_data["job"])
+
+    def test_update_patch(self):
+        user_data = {
+            "name": "test",
+            "job": "tester"
+        }
+
+        self.response = requests.put(self.url + 'api/users/2', json=user_data)
+        self.assertEqual(self.response.status_code, 200)
+
+        expected_user_data = self.response.json()
+        self.assertEqual(expected_user_data["name"], user_data["name"])
+        self.assertEqual(expected_user_data["job"], user_data["job"])
+
+    def test_delete(self):
+        self.response = requests.delete(self.url + 'api/users/2')
+        self.assertEqual(self.response.status_code, 204)
+
+
+class TestRegLog(unittest.TestCase):
+    def setUp(self):
+        self.url = 'https://reqres.in/'
+    def test_register_suc(self):
+        user_data = {
+            "email": "eve.holt@reqres.in",
+            "password": "pistol"
+        }
+
+        self.response = requests.post(self.url + 'api/register', json=user_data)
+        self.assertEqual(self.response.status_code, 200)
+
+        expected_response_data = {
+            "id": 4,
+            "token": "QpwL5tke4Pnpja7X4"
+        }
+
+        self.assertDictEqual(self.response.json(), expected_response_data)
+
+    def test_register_unsuc(self):
+        user_data = {
+            "email": "sydney@fife"
+        }
+
+        self.response = requests.post(self.url + 'api/register', json=user_data)
+        self.assertEqual(self.response.status_code, 400)
+
+        expected_response_data = {
+            "error": "Missing password"
+        }
+
+        self.assertDictEqual(self.response.json(), expected_response_data)
+
+    def test_login_suc(self):
+        user_data = {
+            "email": "eve.holt@reqres.in",
+            "password": "cityslicka"
+        }
+
+        self.response = requests.post(self.url + 'api/login', json=user_data)
+        self.assertEqual(self.response.status_code, 200)
+
+        expected_response_data = {
+            "token": "QpwL5tke4Pnpja7X4"
+        }
+
+        self.assertDictEqual(self.response.json(), expected_response_data)
+
+    def test_login_unsuc(self):
+        user_data = {
+            "email": "peter@klaven"
+        }
+
+        self.response = requests.post(self.url + 'api/login', json=user_data)
+        self.assertEqual(self.response.status_code, 400)
+
+        expected_response_data = {
+            "error": "Missing password"
+        }
+
+        self.assertDictEqual(self.response.json(), expected_response_data)
